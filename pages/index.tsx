f3e7103db485ffbe6b9fd4141aa6409660f3bad4
Pabis <gzpi@gft.com>
2022-03-09 14:31:04 +0100

    Testing husky 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
     const a = 'test';
+    const b = 'hmm';
 
     return (
         <div className={styles.container}>
